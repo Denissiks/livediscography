2e8d926de82fa113e668ec81dade9fecec0cfbb1
Denissiks <38209082+Denissiks@users.noreply.github.com>
2018-10-12 13:50:49 +0200

    Creada clase Song (sin constructor ni nada)

---
+++ LiveDiscography/LiveDiscography/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveDiscography
+{
+    class Song
+    {
+        string songName;
+        Album songAlbum;
+        Artist songArtist;
+        ArrayList featuredArtists;
+        int minLength;
+        int secLength;
+        string genre;
+    }
+}
