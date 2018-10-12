cda393c964fe0fb722ffd3699c0565d518114a6c
Denissiks <38209082+Denissiks@users.noreply.github.com>
2018-10-12 13:16:44 +0200

    Creados constructor, sets y gets de la clase Artist

--- LiveDiscography/LiveDiscography/Artist.cs
+++ LiveDiscography/LiveDiscography/Artist.cs
@@ -9,7 +9,21 @@ namespace LiveDiscography
     class Artist
     {
         string name;
+        string genre;
+        string labels;
         string realName;
 
+        public Artist(string name, string genre, string labels, string realName)
+        {
+            this.Name = name;
+            this.Genre = genre;
+            this.Labels = labels;
+            this.RealName = realName;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Genre { get => genre; set => genre = value; }
+        public string Labels { get => labels; set => labels = value; }
+        public string RealName { get => realName; set => realName = value; }
     }
 }
